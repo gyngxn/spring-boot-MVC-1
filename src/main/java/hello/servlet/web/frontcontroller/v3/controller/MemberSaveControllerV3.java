e3f0f8a3a248d19a87a3c4e1765a0b8921e70fd0
gyngxn_ <jdj0311028@naver.com>
2025-08-14 11:58:04 +0900

    session 5

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -0,0 +1,30 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+
+        // 파라미터 정보는 map에 있음. map에서 필요한 요청 파라미터를 조회
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelView mv = new ModelView("save-result");
+
+        // 모델은 단순한 map 이라서 모델에 뷰에서 필요한 member 객체를 담고 반환
+        mv.getModel().put("member", member);
+        return mv;
+    }
+}
